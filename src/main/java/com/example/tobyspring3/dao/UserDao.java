d17870b6963829b702ab8a60238ddbbe5c417be2
yezyaa <iyeahji@gmail.com>
2023-05-25 03:51:19 +0900

    feat: 생성자를 이용한 DI - UserDao 수정

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -7,8 +7,8 @@ import java.sql.*;
 public class UserDao {
     ConnectionMaker connectionMaker;
 
-    public UserDao() {
-        this.connectionMaker = new DConnectionMaker();
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
@@ -48,14 +48,15 @@ public class UserDao {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
+        ConnectionMaker cm = new DConnectionMaker();
+        UserDao userDao = new UserDao(cm);
         User user = new User();
-        user.setId("5");
-        user.setName("hoho");
-        user.setPassword("1234");
+        user.setId("6");
+        user.setName("banana");
+        user.setPassword("5678");
         userDao.add(user);
 
-        User selectedUser = userDao.get("5");
+        User selectedUser = userDao.get("6");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
